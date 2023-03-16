0fdc1a914e5519b412f012ed8e338ef6ba42fa59
Saiful182 <saifulislamcse0@gamil.com>
2023-03-16 12:47:51 +0600

    player expence part done

--- css/JS/calculation.js
+++ css/JS/calculation.js
@@ -4,6 +4,12 @@ function getInputValue(inputId) {
     const inputDigits = parseFloat(inputValue);
     return inputDigits.toFixed(2);
 }
+function getInnerTextValue(inputId) {
+    const innetTextString = document.getElementById(inputId);
+    const innerTextValue = innetTextString.innerText;
+    const innerTextDigits = parseFloat(innerTextValue);
+    return innerTextDigits.toFixed(2);
+}
 
 document.getElementById('calculte-button').addEventListener('click', function () {
     const numberOfSelectedPlayers = document.getElementById('orderded-list').children.length;
@@ -13,7 +19,22 @@ document.getElementById('calculte-button').addEventListener('click', function ()
         document.getElementById('per-player-cost').value = '';
     }
     else {
-
+        const totatPlayerCost = numberOfSelectedPlayers * playerCost;
+        document.getElementById('player-expences').innerText = totatPlayerCost;
+        document.getElementById('per-player-cost').value = '';
     }
 
+})
+document.getElementById('calculate-total-button').addEventListener('click', function () {
+    const totalPlayerExpence = getInnerTextValue('player-expences');
+    const managerExpence = getInputValue('manager');
+    const coachExpence = getInputValue('coach');
+
+    if (isNaN(totalPlayerExpence && managerExpence && coachExpence)) {
+        const totalCosting = totalPlayerExpence + managerExpence + coachExpence;
+        document.getElementById('total').innerText = totalCosting;
+    }
+    else {
+        alert('please type any valid number in the box')
+    }
 })
